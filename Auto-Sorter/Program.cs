0d270b6e9495e143ce3b13258fef43d814b6fb13
xCrypto1337 <dev.crypto@hotmail.com>
2023-04-22 15:44:24 +0200

    init

---
+++ Auto-Sorter/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileSorter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Auto-Sorter by xCrypto1337\n");
+
+            // Set the directory paths
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var inputDirectory = Path.Combine(baseDirectory, "input");
+            var outputDirectory = Path.Combine(baseDirectory, "output");
+
+            // Create the input and output directories if they don't exist
+            Directory.CreateDirectory(inputDirectory);
+            Directory.CreateDirectory(outputDirectory);
+
+            // Get all the files in the input directory and its subdirectories
+            var allFilesInInputDirectory = Directory.GetFiles(inputDirectory, "*", SearchOption.AllDirectories);
+
+            // Initialize the progress bar
+            var totalFiles = 0;
+            var progressLock = new object();
+            Console.WriteLine($"Sorting {allFilesInInputDirectory.Length} files...\n");
+
+            // Setup cancellation token
+            using var cancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                cancellationTokenSource.Cancel();
+            };
+
+            try
+            {
+                await Task.Run(() => Parallel.ForEach(allFilesInInputDirectory, new ParallelOptions { CancellationToken = cancellationTokenSource.Token }, filePath =>
+                {
+                    // Get the file extension
+                    var fileExtension = Path.GetExtension(filePath).ToLower();
+
+                    // Skip files without extensions
+                    if (string.IsNullOrEmpty(fileExtension))
+                    {
+                        return;
+                    }
+
+                    // Create a new subdirectory in the output directory with the name of the file extension
+                    var outputSubdirectory = Path.Combine(outputDirectory, fileExtension.TrimStart('.'));
+                    Directory.CreateDirectory(outputSubdirectory);
+
+                    // Copy the file to the appropriate subdirectory in the output directory
+                    var outputFilePath = Path.Combine(outputSubdirectory, Path.GetFileNameWithoutExtension(filePath) + fileExtension);
+
+                    try
+                    {
+                        File.Copy(filePath, outputFilePath, overwrite: true);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine($"Error copying file {filePath}: {e.Message}");
+                    }
+
+                    // Update the progress bar
+                    lock (progressLock)
+                    {
+                        totalFiles++;
+                        Console.Write($"\rProcessed {totalFiles} of {allFilesInInputDirectory.Length} files...");
+                    }
+
+                    // Check for cancellation request
+                    cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                }));
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("\nOperation cancelled.");
+            }
+
+            // Clear the progress bar
+            Console.WriteLine("\nDone!");
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+🌟🌈🦄 The Insane Auto-Sorter 🦄🌈🌟
+Looking for a sorting tool that's out of this world? Look no further than the Insane Auto-Sorter! This is not your grandma's file sorting tool - this is a tool that will take you on a wild ride through the world of file organization!
+
+📂 Features
+Automatically sorts all files in the input directory and its subdirectories
+Creates a new directory for each file type and moves the files there
+Supports cancelling the operation at any time
+🚀 Getting Started
+First things first, make sure you're wearing your wildest outfit!
+Clone the repository and prepare for a journey of a lifetime!
+Build the application using your preferred method (e.g., Visual Studio, dotnet CLI)
+Put all the files you want to sort in the input directory
+Say goodbye to boredom and run the FileSorter.exe application!
+Watch in amazement as your files are sorted in a way that defies the laws of physics!
+⚠️ Warning
+This application is not for the faint of heart! It's so wild, it might make your head spin! Use at your own risk and buckle up for a wild ride!
+
+❗ Known Issues
+None, because this application is too insane to have any issues!
+
+🎉 Contributing
+If you have an idea for making this application even crazier, feel free to submit a pull request! We're always looking for ways to take things to the next level.
+
+🕶️ License
+This application is licensed under the MIT License. Use it however you want, but beware - you might never be the same again after experiencing the Insane Auto-Sorter!
